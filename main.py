64de97a22548e48c2e0f99de7cc9a9effb11a565
Matevz Morato <matevz.morato@gmail.com>
2023-05-22 21:30:31 +0200

    Move to on host syncing

--- main.py
+++ main.py
@@ -4,7 +4,10 @@ import cv2
 import depthai as dai
 import contextlib
 from  argparse import ArgumentParser
+import itertools
+from collections import deque 
 from pathlib import Path
+import time
 
 parser = ArgumentParser()
 parser.add_argument("-fps", "--fps", default=3, type=int, required=False,
@@ -36,10 +39,98 @@ mapSocketsStr = {
 fourcc = cv2.VideoWriter_fourcc(*'I420')
 sockets = [dai.CameraBoardSocket.LEFT, dai.CameraBoardSocket.RIGHT, dai.CameraBoardSocket.VERTICAL]
 
+
+class MessageSync:
+    def __init__(self, num_queues, min_diff_timestamp, max_num_messages=4, min_queue_depth=3):
+        self.num_queues = num_queues
+        self.min_diff_timestamp = min_diff_timestamp
+        self.max_num_messages = max_num_messages
+        # self.queues = [deque() for _ in range(num_queues)]
+        self.queues = dict()
+        self.queue_depth = min_queue_depth
+        # self.earliest_ts = {}
+
+    def add_msg(self, name, msg):
+        if name not in self.queues:
+            self.queues[name] = deque(maxlen=self.max_num_messages)
+        self.queues[name].append(msg)
+        # if msg.getTimestampDevice() < self.earliest_ts:
+        #     self.earliest_ts = {name: msg.getTimestampDevice()}
+
+        # print('Queues: ', end='')
+        # for name in self.queues.keys():
+        #     print('\t: ', name, end='')
+        #     print(self.queues[name], end=', ')
+        #     print()
+        # print()
+
+    def get_synced(self):
+
+        # Atleast 3 messages should be buffered
+        min_len = min([len(queue) for queue in self.queues.values()])
+        if min_len == 0:
+            # print('Status:', 'exited due to min len == 0', self.queues)
+            return None
+
+        # initializing list of list 
+        queue_lengths = []
+        for name in self.queues.keys():
+            queue_lengths.append(range(0, len(self.queues[name])))
+        permutations = list(itertools.product(*queue_lengths))
+        # print ("All possible permutations are : " +  str(permutations))
+
+        # Return a best combination after being atleast 3 messages deep for all queues
+        min_ts_diff = None
+        for indicies in permutations:
+            tmp = {}
+            i = 0
+            for n in self.queues.keys():
+                tmp[n] = indicies[i]
+                i = i + 1
+            indicies = tmp
+
+            acc_diff = 0.0
+            min_ts = None
+            for name in indicies.keys():
+                msg = self.queues[name][indicies[name]]
+                if min_ts is None:
+                    min_ts = msg.getTimestampDevice().total_seconds()
+            for name in indicies.keys():
+                msg = self.queues[name][indicies[name]]
+                acc_diff = acc_diff + abs(min_ts - msg.getTimestampDevice().total_seconds())
+
+            # Mark minimum
+            if min_ts_diff is None or (acc_diff < min_ts_diff['ts'] and abs(acc_diff - min_ts_diff['ts']) > 0.0001):
+                min_ts_diff = {'ts': acc_diff, 'indicies': indicies.copy()}
+                # print('new minimum:', min_ts_diff, 'min required:', self.min_diff_timestamp)
+
+            if min_ts_diff['ts'] < self.min_diff_timestamp:
+                # Check if atleast 5 messages deep
+                min_queue_depth = None
+                for name in indicies.keys():
+                    if min_queue_depth is None or indicies[name] < min_queue_depth:
+                        min_queue_depth = indicies[name]
+                if min_queue_depth >= self.queue_depth:
+                    # Retrieve and pop the others
+                    synced = {}
+                    for name in indicies.keys():
+                        synced[name] = self.queues[name][min_ts_diff['indicies'][name]]
+                        # pop out the older messages
+                        for i in range(0, min_ts_diff['indicies'][name]+1):
+                            self.queues[name].popleft()
+
+                    # print('Returning synced messages with error:', min_ts_diff['ts'], min_ts_diff['indicies'])
+                    return synced
+
+        # print('
+
+
+
+
 def createPipeline():
     # Start defining a pipeline
     pipeline = dai.Pipeline()
-    syncNode = pipeline.create(dai.node.Sync)
+    # syncNode = pipeline.create(dai.node.Sync)
     streamNames = []
     for socket in sockets:
         cam = pipeline.create(dai.node.ColorCamera)
@@ -50,8 +141,8 @@ def createPipeline():
         xoutRgb = pipeline.create(dai.node.XLinkOut)
         xoutRgb.setStreamName(str(socket))
         streamNames.append(xoutRgb.getStreamName())
-        cam.isp.link(syncNode.inputs[str(socket)])
-        syncNode.outputs[str(socket)].link(xoutRgb.input)
+        cam.isp.link(xoutRgb.input)
+        # syncNode.outputs[str(socket)].link(xoutRgb.input)
 
     return pipeline, streamNames
 
@@ -91,7 +182,7 @@ with contextlib.ExitStack() as stack:
         basePath = get_base_path(mxId, distancesToRunOn[0])
         basePath.mkdir(parents=True, exist_ok=True)
         for name in streamNames:
-            queue = device.getOutputQueue(name, 4, blocking=True)
+            queue = device.getOutputQueue(name, 1, blocking=False)
             outputQueues[name] = queue
             videoPath = basePath / f"{mapSocketsStr[name]}.avi"
             videoWriters[mxId][name] = cv2.VideoWriter(str(videoPath.absolute()), fourcc, args.fps, (1920, 1200))
@@ -100,23 +191,32 @@ with contextlib.ExitStack() as stack:
     distanceId = 0
     recording = False
     print(f"First measurement needs to be on {distancesToRunOn[distanceId]}m")
+    syncMessages = MessageSync(3, 0.003, 20, 2)
     while True:
-        if recording:
-            frameCount += 1
         for mxId in mxIDs:
             frames = {}
             framesList = []
             for stream in allQueues[mxId].keys():
-                frames[stream] = allQueues[mxId][stream].get().getCvFrame()
-                framesList.append(frames[stream])
+                time.sleep(0.001)
+                frame = allQueues[mxId][stream].tryGet()
+                if frame is not None:
+                    syncMessages.add_msg(stream, frame)
+                frames = syncMessages.get_synced()
+                if frames is None:
+                    continue
+                if recording:
+                    frameCount += 1
+                for key in frames.keys():
+                    frames[key] = frames[key].getCvFrame()
+                    framesList.append(frames[key])
+                mergedImage = cv2.hconcat(framesList)
+                mergedImage = cv2.resize(mergedImage, (900, 150))
+                cv2.imshow(mxId, mergedImage)
                 if not recording:
                     continue
                 if frameCount < 10:
                     videoWriters[mxId][stream].write(frames[stream])
 
-            mergedImage = cv2.hconcat(framesList)
-            mergedImage = cv2.resize(mergedImage, (900, 150))
-            cv2.imshow(mxId, mergedImage)
         if frameCount >= 10:
             recording = False
             frameCount = 0
